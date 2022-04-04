a0f377494376940ea10feaf502c11556981104c2
nabilk11 <khan.nabiln11@gmail.com>
2022-04-04 12:17:52 -0400

    added logoutview directly to urls page

--- main_app/urls.py
+++ main_app/urls.py
@@ -1,13 +1,15 @@
-from unicodedata import name
 from django.urls import path
 from . import views
+from django.contrib.auth.views import LogoutView
 
 
 urlpatterns = [
-    # AUTH URLS
+    # AUTH URLS - Login + Register
     path('login/', views.Login.as_view(), name="login"),
     path('register/', views.Register.as_view(), name="register"),
-
+    #LogoutView imported straight into urls.py
+    path('logout/', LogoutView.as_view(next_page='home'), name='logout'),
+    #Other Paths
     path('', views.Home.as_view(), name="home" ),
     path('about/', views.About.as_view(), name="about" ),
     path('calendar/', views.CalendarView.as_view(), name="calendar" ),
