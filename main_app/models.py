6d4b22bf112828a7d3a89fe69b602164b29b6c6a
nabilk11 <khan.nabiln11@gmail.com>
2022-04-09 14:56:20 -0400

    added basic user profile template, and configured user profile image uplaoding functionality - pf imgs can be rendered

--- main_app/models.py
+++ main_app/models.py
@@ -12,6 +12,12 @@ from phone_field import PhoneField
 class Profile(models.Model):
     user = models.OneToOneField(User, null=True, on_delete=models.CASCADE)
     bio = models.TextField()
+    profile_pic = models.ImageField(null=True, blank=True, upload_to="profile_imgs")
+    website_url = models.CharField(max_length=255, null=True, blank=True)
+    instagram_url = models.CharField(max_length=255, null=True, blank=True)
+    twitter_url = models.CharField(max_length=255, null=True, blank=True)
+    linkedin_url = models.CharField(max_length=255, null=True, blank=True)
+
 
     def __str__(self):
         return str(self.user)
